594666a1a9b953d16a961f1a5228544eb8fa8f48
Alcides Wenner <alcideswenner.jus@gmail.com>
2022-02-20 14:22:26 -0300

    aaasssksjskkj

--- src/main/java/com/example/docker_spring/controlles/Teste.java
+++ src/main/java/com/example/docker_spring/controlles/Teste.java
@@ -1,7 +1,15 @@
 package com.example.docker_spring.controlles;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +19,11 @@ public class Teste {
     @GetMapping
     public String teste2(){
         return "tudo bem?";
+    }
+
+    @GetMapping(value ="/image",produces  = MediaType.IMAGE_PNG_VALUE)
+    public  byte[] getImageWithMediaType() throws IOException {
+        File file=new File("target/image.png");
+        return Files.readAllBytes(file.toPath());
     }
 }
